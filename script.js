7eb69bb2bdc470815d4d23616d2ba9f4be20bdad
Steven Polanco <6752411+spolanco94@users.noreply.github.com>
2025-03-09 19:04:05 -0400

    Add images to site
    Include images for Activities
    Include images for Reviewers
    Include images for Transportation
    Add functionality for download button to trigger image download

--- script.js
+++ script.js
@@ -3,18 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const hiddenActivities = document.querySelectorAll(".activity.hidden");
     let activitiesPerClick = 3;
 
-    loadMoreButton.addEventListener("click", function () {
-        let count = 0;
+    // Load more activities button
+    if(loadMoreButton) {
+        loadMoreButton.addEventListener("click", function () {
+            let count = 0;
 
-        hiddenActivities.forEach(activity => {
-            if (count < activitiesPerClick && activity.classList.contains("hidden")) {
-                activity.classList.remove("hidden");
-                count++;
+            hiddenActivities.forEach(activity => {
+                if (count < activitiesPerClick && activity.classList.contains("hidden")) {
+                    activity.classList.remove("hidden");
+                    count++;
+                }
+            });
+
+            if(document.querySelectorAll(".activity.hidden").length === 0) {
+                loadMoreButton.style.display = "none";
             }
         });
+    }
 
-        if(document.querySelectorAll(".activity.hidden").length === 0) {
-            loadMoreButton.style.display = "none";
-        }
-    });
+    // Download map button
+    const downloadButton = document.getElementById("download-map");
+    if (downloadButton) {
+        downloadButton.addEventListener("click", function () {
+            alert("Your offline map is downloading! (Photo by Andrew Stutesman on Unsplash)");
+        });
+    }
 });
